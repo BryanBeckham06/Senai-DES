733e43d1a48b9b153d60fcca9866919881b33e89
Carla-coder <cmartezanato@yahoo.com.br>
2024-05-21 10:21:41 -0300

    Atts

---
+++ 2 Des/02-pwfe/Aula 13/Quizapp/questions.js
@@ -0,0 +1,43 @@
+export default [
+    {
+        question: "Em qual ano foi lançado o primeiro Air Jordan?",
+        answers: [
+            {option: "1995", correct:false},
+            {option: "1986", correct:false},
+            {option: "1980", correct:false},
+            {option: "1985", correct:true},
+        ],
+    },
+
+    {
+        question: "Qual foi o faturamento do primeiro lancamento do primeiro Air Jordan ?",
+        answers: [
+            {option: "62 Milhoes de Dolares", correct:false},
+            {option: "102 Milhoes de Dolares", correct:false},
+            {option: "126 Milhoes de Dolares", correct:true},
+            {option: "93 Milhoes de Dolares", correct:false},
+
+        ],
+    },
+    {
+        question: "Qual foi o Valor pago pela nike na sua atual e mundialmente conhecida logo?",
+        answers: [
+            {option: "$20.000", correct:false},
+            {option: "$35", correct:true},
+            {option: "$220", correct:false},
+            {option: "$500", correct:false},
+
+        ],
+    },
+    {
+        question: "Qual marca Michael Jordan assinaria caso não fechasse com a Nike?",
+        answers: [
+            {option: "Converse", correct:false},
+            {option: "New Balance", correct:false},
+            {option: "Adidas", correct:true},
+            {option: "Puma", correct:false},
+
+        ],
+    },
+    
+]
